804f3c5e1c81d87a0e9bab8248eb181b090db664
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2025-11-12 13:50:22 +0100

    patch 2

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, render_template
+from services.anonimize import anonimizacja
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    anonimized_text = None
+    original_text = ""
+    anonimized_text = "" 
     if request.method == 'POST':
-        anonimized_text = request.form.get('user_text') # Placeholder for actual anonimization logic
-    return render_template("prompt_input_form.html", anonimized_text=anonimized_text)
+        original_text = request.form.get('user_text', '')
+        anonimized_text = anonimizacja(original_text)
+    return render_template(
+        "prompt_input_form.html",
+        original_text=original_text,
+        anonimized_text=anonimized_text
+    )
